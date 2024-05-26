bdeb06e3b186c43f908afb345e9a020a233d0274
Melquisedec Felipe Coutinho <melquisedec.coutinho@mercadolivre.com>
2024-05-26 19:49:10 -0300

    Update icon component size

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,5 +1,5 @@
 export default function Icon() {
   return (
-    <div className="size-8 bg-gradient-to-r from-violet-500 to-violet-600 dark:from-violet-400 dark:to-violet-500 grid place-content-center rounded-full" />
+    <div className="size-6 md:size-8 bg-gradient-to-r from-violet-500 to-violet-600 dark:from-violet-400 dark:to-violet-500 grid place-content-center rounded-full" />
   )
 }
