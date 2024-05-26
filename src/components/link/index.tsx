8882eeac8d14dd0bd041dd0c039d252434a63848
Melquisedec Felipe Coutinho <melquisedec.coutinho@mercadolivre.com>
2024-05-25 22:12:08 -0300

    Update icon and link components styling and accessibility

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -13,7 +13,7 @@ export default function Link({ children, className, href }: LinkProps) {
   return (
     <NextLink
       className={cn(
-        'hover:underline inline-flex items-center w-max transition-all break-words hover:text-foreground text-violet-500 hover:text-violet-600 dark:text-violet-400 dark:hover:text-violet-500',
+        'inline-flex items-center w-max transition-all break-words hover:text-foreground text-violet-500 hover:text-violet-600 dark:text-violet-400 dark:hover:text-violet-500',
         className
       )}
       href={href}
