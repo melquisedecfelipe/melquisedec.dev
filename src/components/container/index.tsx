5742211868e83f8329205de62dc11c5c497aa5bd
Melquisedec Felipe Coutinho <melquisedec.coutinho@mercadolivre.com>
2025-02-05 10:19:11 -0300

    Make container full screen height on all devices

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <main className="max-w-[700px] m-auto p-5 md:p-10 lg:p-20 md:h-screen">
+    <main className="max-w-[700px] m-auto p-5 md:p-10 lg:p-20 h-screen">
       {children}
     </main>
   )
